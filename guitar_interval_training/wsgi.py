190272ff8149135d8bb7df741d4676a01374a148
suehiro <nemunemu_ya@yahoo.co.jp>
2021-08-31 23:05:21 +0900

    First Commit

---
+++ guitar_interval_training/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for guitar_interval_training project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guitar_interval_training.settings')
+
+application = get_wsgi_application()
